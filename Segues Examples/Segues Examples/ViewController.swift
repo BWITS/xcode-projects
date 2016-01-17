d7bb32656f00c5069bd519adbb36902f119c7909
Bill Wang <ozbillwang@gmail.com>
2016-01-17 12:50:41 +1100

    Segues Examples

---
+++ Segues Examples/Segues Examples/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Segues Examples
+//
+//  Created by Bill W on 17/01/2016.
+//  Copyright © 2016 AppFish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
