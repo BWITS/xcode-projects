243f6f52b7b6114d0dc6431057f5dfbd312b874b
Bill Wang <ozbillwang@gmail.com>
2016-01-17 13:21:29 +1100

    keyboard control - Cat ages

--- Cat years/Cat years/ViewController.swift
+++ Cat years/Cat years/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var catAgeTextField: UITextField!
     @IBOutlet weak var resultLable: UILabel!
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.catAgeTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 }
 
